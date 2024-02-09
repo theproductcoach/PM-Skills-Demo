54427d3a7ab375031829ede4ab2ca92ece514a27
theproductcoach <will@productcoach.io>
2024-02-09 10:37:28 +0000

    Neural Networks!

--- Machine Learning/Deep Learning/Neural Networks/My First Neural Network.py
+++ Machine Learning/Deep Learning/Neural Networks/My First Neural Network.py
@@ -1,10 +1,21 @@
 
 # Inputs from the last layer
-inputs = [1.2, 5.1, 2.1] 
-weights = [3.1, 2.1, 8.7]
-bias = 3
+inputs = [1, 2, 3, 2.5] 
 
-# Calculate the output of the neuron
-output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + bias
+weights = [[0.2, 0.8 ,-0.5, 1], 
+           [0.5, -0.91, 0.26, -0.5], 
+           [-0.26, -0.27, 0.17, 0.87]]
 
-print(output)
+biases = [2, 3, 0.5]
+
+
+# # Calculate the output of the neuron
+# layer_outputs = [] # Output of the current layer
+# for neuron_weights, neuron_bias in zip(weights, biases):
+#     neuron_output = 0 # Output of a given neuron
+#     for n_input, weight in zip(inputs, neuron_weights):
+#         neuron_output += n_input*weight
+#     neuron_output += neuron_bias
+#     layer_outputs.append(neuron_output)
+
+# print(layer_outputs)
