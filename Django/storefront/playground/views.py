fd14b0463aceabb801896bca55879150685dbfe3
theproductcoach <will@productcoach.io>
2024-02-19 14:37:33 +0000

    First view path

--- Django/storefront/playground/views.py
+++ Django/storefront/playground/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+# Views are essentially request handlers.
+
+def say_hello(request):
+    return HttpResponse('Hello World')
