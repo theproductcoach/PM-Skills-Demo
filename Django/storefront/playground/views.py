ec08ad2094606a1502ecd0098a834d75a162142c
theproductcoach <will@productcoach.io>
2024-02-20 13:05:23 +0000

    Django stuff

--- Django/storefront/playground/views.py
+++ Django/storefront/playground/views.py
@@ -4,5 +4,11 @@ from django.http import HttpResponse
 # Create your views here.
 # Views are essentially request handlers.
 
+def calculate():
+    x = 1 
+    y = 2
+    return x
+
 def say_hello(request):
-    return HttpResponse('Hello World')
+    x = calculate()
+    return render(request, 'hello.html', {'name': 'Will'})
