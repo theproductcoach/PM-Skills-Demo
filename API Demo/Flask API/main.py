8ceee6cf89b2d4f398482286e8baba3ccaa3daba
theproductcoach <will@productcoach.io>
2024-03-24 14:03:22 +0000

    Last Flask API stuff

--- API Demo/Flask API/main.py
+++ API Demo/Flask API/main.py
@@ -22,6 +22,11 @@ video_put_args.add_argument("name", type=str, help="Name of the video is require
 video_put_args.add_argument("views", type=int, help="Views of the video is required", required=True)
 video_put_args.add_argument("likes", type=int, help="Likes on the video is required", required=True)
 
+video_update_args = reqparse.RequestParser()
+video_update_args.add_argument("name", type=str, help="Name of the video is required")
+video_update_args.add_argument("views", type=int, help="Views of the video is required")
+video_update_args.add_argument("likes", type=int, help="Likes on the video is required")
+
 resource_fields = {
     'id': fields.Integer,
     'name': fields.String,
@@ -47,6 +52,25 @@ class Video(Resource):
         db.session.add(video)
         db.session.commit()
         return video, 201
+    
+    @marshal_with(resource_fields)
+    def patch(self, video_id):
+        args = video_update_args.parse_args()
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="Video doesn't exist, cannot update")
+        
+        if args['name']:
+            result.name = args['name']
+        if args['views']:
+            result.name = args['views']
+        if args['likes']:
+            result.name = args['likes']
+
+        db.session.commit()
+
+        return result
+
  
     def delete(self, video_id):
         abort_if_video_id_doesnt_exist(video_id)
--- API Demo/Flask API/test.py
+++ API Demo/Flask API/test.py
@@ -2,7 +2,8 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-data = [{"likes": 78, "name": "Joe", "views": 100000}, 
+
+'''data = [{"likes": 78, "name": "Joe", "views": 100000}, 
         {"likes": 10000, "name": "How to make REST API", "views": 80000}, 
         {"likes": 35, "name": "Tim", "views": 2000}]
 
@@ -13,4 +14,7 @@ for i in range(len(data)):
 input()
 
 response = requests.get(BASE + "video/6")
+print(response.json())'''
+
+response = requests.patch(BASE + "video/2", {"views":99, "likes":101})
 print(response.json())
