bc5500dbc45dc81e3704ae79c042e7c76d525f9a
theproductcoach <will@productcoach.io>
2024-03-24 17:39:46 +0000

    NodeJS with MongoDB is a huge headache, getting there though.

--- API Demos/Node API/server.js
+++ API Demos/Node API/server.js
@@ -1,24 +1,29 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
-const port = 3000
 
 //routes
 
 app.get('/', (req, res) => {
-    res.send('Hello Node API')
-})
-
-app.listen(3000, ()=> {
-    console.log('Node API app is running on port 3000')
+    res.send('Hello Node API Testing')
 })
 
 
-/*
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-*/
+mongoose.connect('mongodb+srv://admin:12345678Admin@theproductcoachapi.nvkemdd.mongodb.net/Node-API?retryWrites=true&w=majority&appName=TheProductCoachAPI')
+.then(() => {
+    console.log('connected to MongoDB')
+    app.listen(3000, ()=> {
+        console.log('Node API app is running on port 3000')
+    });
+}).catch((error) => {
+        console.error('MongoDB connection error:', error);
+    });
+
+
+
+
+
+//gQNiOALus0rGrqlv
+
+//82.13.115.46
