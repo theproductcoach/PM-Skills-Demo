de695a3f05a1dfdbbe5e302c1736d465512b1452
theproductcoach <will@productcoach.io>
2024-03-25 15:48:58 +0000

    Lots of Node.js updates, onto Python websites now.

--- API Demos/Node API/server.js
+++ API Demos/Node API/server.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Product = require('./models/productModel')
 const app = express()
+require('dotenv').config()
+
+
+app.use(express.json())
 
 //routes
 
@@ -10,7 +15,70 @@ app.get('/', (req, res) => {
 
 
 
-mongoose.connect('mongodb+srv://admin:12345678Admin@theproductcoachapi.nvkemdd.mongodb.net/Node-API?retryWrites=true&w=majority&appName=TheProductCoachAPI')
+//Get all products
+app.get('/products', async(req, res) => {
+    try {
+        const product = await Product.find({})
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+//Get a single product
+app.get('/products/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+//Push Products
+app.post('/products', async(req, res) => {
+    try {
+        const product = await Product.create(req.body)
+        res.status(200).json(product);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+    }
+})
+
+//Update a product
+app.put('/products/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+        if(!product) {
+            return res.status(404).json({message: `cannot find any product with ID ${id}`})
+            //if we can't find it in the DB
+        }
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+//Delete a product
+app.delete('/products/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id);
+        if(!product) {
+            return res.status(404).json({message: `cannot find any product with ID ${id}`})
+            //if we can't find it in the DB
+        }
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+mongoose.connect(`mongodb+srv://admin:${process.env.MONGODB_PASSWORD}@theproductcoachapi.nvkemdd.mongodb.net/Node-API?retryWrites=true&w=majority&appName=TheProductCoachAPI`)
 .then(() => {
     console.log('connected to MongoDB')
     app.listen(3000, ()=> {
@@ -21,9 +89,6 @@ mongoose.connect('mongodb+srv://admin:12345678Admin@theproductcoachapi.nvkemdd.m
     });
 
 
-
-
-
-//gQNiOALus0rGrqlv
-
-//82.13.115.46
+//app.post('/product', (req, res) => {
+//    console.log(req.body)
+//    res.send(req.body)
