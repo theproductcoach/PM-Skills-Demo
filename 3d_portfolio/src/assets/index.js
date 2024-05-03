fcee9480a4521a5e7abcc32d3ceb76967b02bd24
theproductcoach <will@productcoach.io>
2024-05-03 13:14:22 +0100

    More portfolio updates

--- 3d_portfolio/src/assets/index.js
+++ 3d_portfolio/src/assets/index.js
@@ -6,6 +6,8 @@ import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
+import linkout from "./linkout.svg";
+
 
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
@@ -71,5 +73,6 @@ export {
   ladbrokes,
   carrent,
   jobit,
-  tripguide
+  tripguide,
+  linkout
 };
