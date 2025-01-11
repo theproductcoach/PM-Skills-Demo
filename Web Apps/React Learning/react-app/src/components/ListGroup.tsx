996d5fa215bef81d80e07b08c9d71ae4fd716870
theproductcoach <will@productcoach.io>
2025-01-11 09:38:01 +0000

    Learning how to handle click (and other) events, typing them etc.

--- Web Apps/React Learning/react-app/src/components/ListGroup.tsx
+++ Web Apps/React Learning/react-app/src/components/ListGroup.tsx
@@ -1,16 +1,25 @@
 import App from "../App";
+import { MouseEvent } from "react";
 
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
  
+  // Event handler
+  const handleClick = (event: MouseEvent) => console.log(event)
 
   return (
     <>
       <h1>List</h1>
-      {items.length === 0 && <p>No item found</p> }
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={handleClick}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
