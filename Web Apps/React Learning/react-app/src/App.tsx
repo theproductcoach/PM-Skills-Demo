88b6e9cc004b9394aad843fc6c2980299b8ee491
theproductcoach <will@productcoach.io>
2025-01-11 16:18:56 +0000

    Finished Button and alert components for today

--- Web Apps/React Learning/react-app/src/App.tsx
+++ Web Apps/React Learning/react-app/src/App.tsx
@@ -1,16 +1,15 @@
-import ListGroup from "./components/ListGroup";
-
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+    const [alertVisible, setAlertVisibility] = useState(false);
 
-  const handleSelectItem = (item: string) => {
-    console.log(item)
-  }
 
   return (
     <div>
-      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />
+      { alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>}
+      <Button color='primary' onClick={() => setAlertVisibility(true)}>My Button</Button>
     </div>
   );
 }
