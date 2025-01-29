db902b3704cd930490533df8077c64b7a1ed933d
theproductcoach <will@productcoach.io>
2025-01-29 12:38:27 +0000

    Cleanup

--- AI/Langraph RAG/rag_with_langgraph.py
+++ AI/Langraph RAG/rag_with_langgraph.py
@@ -48,7 +48,6 @@ print("Documents successfully indexed.")
 # Define the custom prompt
 template = """Use the following pieces of context to answer the question at the end.
 If you don't know the answer, just say that you don't know, don't try to make up an answer.
-Use three sentences maximum and keep the answer as concise as possible.
 Always say "thanks for asking!" at the end of the answer.
 
 {context}
